82768732859d146a0d799c2c4be99313540aeb81
Parks Projets <guigui.97@hotmail.fr>
2018-07-14 12:58:14 +0200

    Clean up code and fix some bugs.

--- flaps-the-mips/src/update/bird.s
+++ flaps-the-mips/src/update/bird.s
@@ -10,66 +10,71 @@
 # Update the bird.
 update_bird:
 
-    li $gamemustend, 0
+    li $gamemustend, 0  # Ensure the game will not end now.
     
 
 
 # Manage the user input
 user_input:
 
-    lw $btns, kSwitchesAddress($zero)
-    srl $btns, 16  # Get buttons value
+    lw $btns, kSwitchesAddress($zero)  # Read switch values.
+    srl $btns, 16  # Get buttons value.
 
-    lw $inputcounter, dBirdInputcounter($zero)
+    lw $inputcounter, dBirdInputcounter($zero)  # Input counter for delay between jumps.
 
-    beq $inputcounter, $zero, user_buttons  # Delay between jumps
-    addi $inputcounter, -1
+    beq $inputcounter, $zero, user_buttons  # If input couter is 0: we can jump!
+    addi $inputcounter, -1  # Else, decrement the counter and don't jump.
     j after_user_input
 
 user_buttons:
 
-    lw $btndown, dBirdBtndown($zero)
-    bne $btndown, $zero, after_user_buttons
+    lw $btndown, dBirdBtndown($zero)  # Read last button state to prevent unwanted jumps.
+    bne $btndown, $zero, after_user_buttons  # Between two jumps we must release the button.
 
-    beq $btns, $zero, after_user_buttons
+    beq $btns, $zero, after_user_buttons  # No buttons pressed.
 
-    beq $gamestarted, $zero, after_user_buttons  # If the game has not started
-    bne $gameended, $zero, after_user_buttons  # If the game has ended
+    beq $gamestarted, $zero, after_user_buttons  # If the game has not started.
+    bne $gameended, $zero, after_user_buttons  # If the game has ended.
 
 jump_bird:
 
-    li $inputcounter, kBirdAllowJump
-    addi $birdvel, (-kBirdJumpHeight)
+    li $inputcounter, kBirdAllowJump  # Reset the jump counter.
+    addi $birdvel, (-kBirdJumpHeight)  # Jump: increment y velocity.
 
 after_user_buttons:
 
-    sw $btns, dBirdBtndown($zero)
+    sw $btns, dBirdBtndown($zero)  # Save button state.
 
 after_user_input:
 
-    sw $inputcounter, dBirdInputcounter($zero)
+    sw $inputcounter, dBirdInputcounter($zero)  # Save input counter.
     
 
 
 # Update the bird velocity
 update_bird_velocity:
 
-    beq $gamestarted, $zero, after_update_bird  # The game has not started
+    beq $gamestarted, $zero, after_update_bird  # The game has not started.
 
     addi $tmp, $birdvel, (-kBirdMaxVelocity)
-    bgez $tmp, update_bird_position  # Minimum aloowed velocity
+    bgez $tmp, update_bird_position  # Maximum allowed velocity (negative).
 
-    addi $birdvel, kBirdGravity
+    addi $birdvel, kBirdGravity  # Apply gravity.
 
 
 
 # Update the bird position
 update_bird_position:
 
-    add $birdy, $birdvel
+    add $birdy, $birdvel  # Apply bird velocity.
 
-    addi $tmp, $birdy, (-(kSceneHeight - sBird0Height - 1) * 4)
+    bgez $birdy, update_bird_position_bottom  # The bird must not go to hight.
+    li $birdy, 0
+    li $birdvel, 0  # Reset y velocity.
 
+update_bird_position_bottom:
+
+    addi $tmp, $birdy, (-(kSceneHeight - sBird0Height - 1) * 4)  # Distance to the ground.
     bltz $tmp, after_update_bird
 
     sub $birdy, $tmp
--- flaps-the-mips/src/update/pipes.s
+++ flaps-the-mips/src/update/pipes.s
@@ -10,19 +10,19 @@
 # Update the pipes.
 update_pipes:
 
-    beq $gamestarted, $zero, after_update_pipes  # If the game not started
+    beq $gamestarted, $zero, after_update_pipes  # If the game not started.
 
-    la $addr, (dPipes)
-    la $endaddr, (dPipes + 8 * kNumberOfPipes)
+    la $addr, (dPipes)  # Address of pipes informations.
+    la $endaddr, (dPipes + 8 * kNumberOfPipes)  # End address.
 
 
-update_pipe_i: # Loop: update the i-th pipe
+update_pipe_i: # Loop: update the i-th pipe.
 
-    lw $pipex, 0($addr)  # Load x pos
-    lw $pipey, 4($addr)  # Load y pos
+    lw $pipex, 0($addr)  # Load x pos.
+    lw $pipey, 4($addr)  # Load y pos.
 
-    bne $gameended, $zero, test_bird_collision  # If the game has ended
-    addi $pipex, $pipex, (-2 * 4)  # Update x pos -> x -= 2
+    bne $gameended, $zero, end_update_pipe_i  # The game has ended: don't move pipes.
+    addi $pipex, (-2 * 4)  # Update pipe x pos -> x -= 2.
 
 
 
@@ -31,41 +31,42 @@ update_pipe_i: # Loop: update the i-th pipe
 test_bird_collision:
 
     # The bird collides the pipe if
-    #   birdx + 17 >= pipex && birdx <= pipex + 24   and
-    #   birdy < pipey || birdy + 12 > pipey + 45
+    #   birdx + 17 >= pipex && birdx <= pipex + 22   and
+    #   birdy < pipey || birdy + 12 > pipey + 42
 
-    sub $xcmp, $pipex, $birdx  # xcmp = pipex - birdx
+    sub $xcmp, $pipex, $birdx  # xcmp = pipex - birdx.
 
     addi $xcmpsh, $xcmp, (-17 * 4)
-    bgtz $xcmpsh, bird_not_collide  # not (pipex - birdx - 17 <= 0)
+    bgtz $xcmpsh, bird_not_collide  # not (pipex - birdx - 17 <= 0).
 
-    addi $xcmpsh, $xcmp, (24 * 4)
-    bltz $xcmpsh, bird_not_collide  # not (pipex - birdx + 24 >= 0)
+    addi $xcmpsh, $xcmp, (22 * 4)
+    bltz $xcmpsh, bird_not_collide  # not (pipex - birdx + 24 >= 0).
 
-    sub $ycmp, $pipey, $birdy  # ycmp = pipey - birdy
-    bgtz $ycmp, bird_collides  # (pipey - birdy > 0)
+    sub $ycmp, $pipey, $birdy  # ycmp = pipey - birdy.
+    bgtz $ycmp, bird_collides  # (pipey - birdy > 0).
 
-    addi $ycmp, $ycmp, ((45 - 12) * 4)
-    bgez $ycmp, bird_not_collide  # not (pipey - y + 45-12 < 0)
+    addi $ycmp, $ycmp, ((42 - 12) * 4)
+    bgez $ycmp, bird_not_collide  # not (pipey - y + 42-12 < 0).
 
 
 bird_collides: # The end of the game is close...
 
     li $gameended, 1
+    li $birdvel, (-kBirdGravity * 7)  # Reset bird velocity for a better result.
 
 
-bird_not_collide: # If the bird doesn't collide the pipe
+bird_not_collide: # If the bird doesn't collide the pipe.
 
 
 
 
-# Replace the pipe if it's on the left
+# Replace the pipe if it's on the left.
 rand_pipe:
 
     addi $tmp, $pipex, (sPipeBodyWidth * 4)
     bgtz $tmp, after_rand_pipe
 
-    li $pipex, ((320 + 6) * 4)  # Reset x position
+    li $pipex, ((320 + 6) * 4)  # Reset x position.
 
 
     # We rand the y position.
@@ -85,8 +86,8 @@ after_rand_pipe:
 # Update the score
 update_score: 
 
-    li $tmp, (150 * 4)
-    bne $pipex, $tmp, after_update_score # Update the score
+    li $tmp, (150 * 4)  # Position of the pipe where we increment the score.
+    bne $pipex, $tmp, after_update_score  # Update the score.
 
     .include "game/score.s"
     .file "update-pipes.s"
@@ -106,6 +107,6 @@ end_update_pipe_i:
     .file "update-pipes.s"
 
     addi $addr, 8  # $addr += 8
-    bne $addr, $endaddr, update_pipe_i
+    bne $addr, $endaddr, update_pipe_i  # Update next pipe.
 
 after_update_pipes:
